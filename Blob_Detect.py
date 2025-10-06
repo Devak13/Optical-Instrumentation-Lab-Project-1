2342df2018d04e39dfdd5cfed3441ffb2a705975
Devak13 <144050076+Devak13@users.noreply.github.com>
2025-10-06 13:43:41 +0530

    Update Blob_Detect.py

--- Blob_Detect.py
+++ Blob_Detect.py
@@ -2,24 +2,24 @@ from PIL import Image, ImageDraw
 import numpy as np
 import math
 from time import sleep
-import blink as ub
+import blink as ub    #invoking function file
 
 # Camera and geometry setup
-ifovx = 0.0211032992   # radians per pixel (horizontal IFOV) with 54.6997 degrees 30.0cm field and distance 29.0cm
-xc = 82.5         # distance between camera and laser
-xR = 2592         # image width
-yR = 1944         # image height
-angle = math.degrees(math.atan(xc/290))    
-tan = math.tan(math.radians(angle))
-yc = xc / tan    
-y1=yc
+ifovx = 0.0211032992                           # radians per pixel (horizontal IFOV) with 54.6997 degrees 30.0cm field and distance 29.0cm
+xc = 82.5                                      # distance between camera and laser
+xR = 2592                                      # image width
+yR = 1944                                      # image height
+angle = math.degrees(math.atan(xc/290))        #Angle between laser and camera    
+tan = math.tan(math.radians(angle))            #tan of angle between laser and camera
+yc = xc / tan                                  #Distance at which laser hits camera center in reference plane
+y1=yc                                          
 
 # Capture image
-image_path = "/home/iap/Pictures/photo1.jpg"
+image_path = "/home/iap/Pictures/photo1.jpg"  
 ub.image_capture(image_path)
 
-# Load image as numpy array
-image = Image.open(image_path).convert("RGB")
+# Load image as numpy array and saving image with rgb values
+image = Image.open(image_path).convert("RGB")         
 width, height = image.size
 print(f"width is {width} and height is {height}")
 
@@ -31,7 +31,7 @@ y_min, y_max = int(yR/2 - 100), int(yR/2 + 300)
 
 roi = np_img[y_min:y_max, x_min:x_max, :]   # sub-image
 
-# Split channels
+# Split rgb channels
 red   = roi[:, :, 0].astype(np.int16)
 green = roi[:, :, 1].astype(np.int16)
 blue  = roi[:, :, 2].astype(np.int16)
@@ -40,7 +40,6 @@ blue  = roi[:, :, 2].astype(np.int16)
 mask = (red > 80) & (red > 1.5*green) & (red > 1.5*blue)
 
 ys, xs = np.where(mask)
-#print(str(ys)+" "+str(xs))   # coordinates within ROI
 
 if len(xs) == 0:
     print("Outside of region or not reading any red pixel")
